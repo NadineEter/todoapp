3ccff6bfe282df7b01ab4acc5a9d0079eaed8616
Nadine Eter <nadine_eter@hotmail.de>
2013-12-10 16:15:25 +0100

    distance_from_now_in_days Methode in Task eingefügt

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -28,6 +28,24 @@ test "is not delayed if deadline is today" do
  task.deadline = Date.today
  assert !task.is_delayed?
 end 
+
+test "destance in days from today" do
+  task = Task.new
+  task.deadline = Date.today
+  assert_equal 0, task.distance_from_now_in_days
+end
+
+test "destance in days from yesterday" do
+  task = Task.new
+  task.deadline = Date.today - 1
+  assert_equal -1, task.distance_from_now_in_days
+end
+
+test "destance in days from tomorrow" do
+  task = Task.new
+  task.deadline = Date.today + 1
+  assert_equal 1, task.distance_from_now_in_days
+end 
   # test "the truth" do
   #   assert true
   # end
