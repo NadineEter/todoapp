f9d977d8b1330f9e6a00f41d74dd3631eb34e2ba
Nadine Eter <nadine_eter@hotmail.de>
2013-12-10 16:09:31 +0100

    is_delayed? Methode in Task eingefügt

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
 	validates :name, presence: true
 	validates :deadline, presence: true
 	validates :duration, presence: true, numericality: true 
+def is_delayed?
+ self.deadline < Date.today
+end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -12,7 +12,22 @@ test "task can be saved with name, deadline and duraton" do
  task.deadline = Date.today + 7.days
  task.duration = 5.0
  assert task.save
-end   
+end  
+test "is delayed" do
+ task = Task.new
+ task.deadline = Date.today - 10.days
+ assert task.is_delayed?
+end
+test "is not delayed" do
+ task = Task.new
+ task.deadline = Date.today + 10.days
+ assert !task.is_delayed?
+end
+test "is not delayed if deadline is today" do
+ task = Task.new
+ task.deadline = Date.today
+ assert !task.is_delayed?
+end 
   # test "the truth" do
   #   assert true
   # end
