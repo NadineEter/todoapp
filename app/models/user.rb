c1366a96e9044bdc971896453c430dae754e3a7c
Nadine Eter <nadine_eter@hotmail.de>
2013-12-17 14:37:11 +0100

    Tasks können geändert vom delegierten User geändert werden

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :tasks
+  has_many :delegated_tasks, class_name: "Task", foreign_key: "delegated_id"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  validates :username, presence:  true, uniqueness: true
