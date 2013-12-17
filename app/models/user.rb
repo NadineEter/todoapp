c4d0aeac294e9211eb87d8e8471cb39f5ecea1c6
Nadine Eter <nadine_eter@hotmail.de>
2013-12-17 12:01:44 +0100

    Username zu User hinzugefügt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   has_many :tasks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+ validates :username, presence:  true, uniqueness: true
 end
