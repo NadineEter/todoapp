6bd227c4750c227eb3e2675e56076907a6ef7748
Nadine Eter <nadine_eter@hotmail.de>
2014-01-07 17:38:25 +0100

    Edit und Update Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,15 @@ def destroy
   @project = Project.find(params[:id])
   @project.destroy
 end
+def edit
+  @project = Project.find(params[:id])
+end
+def update
+  @project = Project.find(params[:id])
+  if !@project.update(project_params)
+    render action: 'error'
+  end
+end
 private
     def project_params
       params.require(:project).permit(:name)
